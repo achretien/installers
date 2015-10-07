5ef4a062c8b83cbf54b53afcb35a4f7b9162181c
Aurélien Chrétien <aurelien@recatch.tv>
2015-10-07 16:52:10 +0200

    discover codesniffer_path

--- src/Composer/Installers/PhpCodeSnifferInstaller.php
+++ src/Composer/Installers/PhpCodeSnifferInstaller.php
@@ -1,9 +1,32 @@
 <?php
 namespace Composer\Installers;
 
+use Composer\Package\Package;
+
 class PhpCodeSnifferInstaller extends BaseInstaller
 {
-    protected $locations = array(
-        'standard' => 'vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/{$name}/',
-    );
+    protected $locations = [
+        'coding-standard' => '{$phpcs_standards_folder}/{$name}',
+    ];
+
+    public function inflectPackageVars($vars)
+    {
+        $phpcsPackage = new Package('squizlabs/php_codesniffer', '2.0', '');
+        $base         = $this->composer->getInstallationManager()->getInstallPath($phpcsPackage);
+
+        if (!$base) {
+            throw new \InvalidArgumentException('Package is not installed: "squizlabs/php_codesniffer"');
+        }
+
+        $subPaths = ['CodeSniffer'.DIRECTORY_SEPARATOR.'Standards'];
+
+        foreach ($subPaths as $subPath) {
+            $path = $base.DIRECTORY_SEPARATOR.$subPath;
+            if (is_dir($path) && is_readable($path)) {
+                $vars['phpcs_standards_folder'] = $path;
+            }
+        }
+
+        return $vars;
+    }
 }
